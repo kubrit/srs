13bea27cafbdb5df7d4b247510773cc9861e2f74
gmbroker <bogumil.kraszewski@gmail.com>
2019-05-06 12:26:53 +0200

    fixed autocomplete

--- scripts/autocomplete_recipient.php
+++ scripts/autocomplete_recipient.php
@@ -1,17 +1,20 @@
 <?php
 include '../core/init.php';
 
-if (isset($_POST['query'])) {
-	
-	$query = $_POST['query'];
-	
-	$sql = mysqli_query($connect,"SELECT * FROM ".$t_shipments." WHERE recipient LIKE '%{$query}%'");
-	
-	$array = array();
-	
-	while($row = mysqli_fetch_assoc($sql)) {
-		array[] = $row['recipient'];
-	}
-	echo json_encode($array);
+$req = "SELECT recipient FROM `" . $t_shipments . "` WHERE recipient LIKE '%".$_REQUEST['term']."%' GROUP BY recipient LIMIT 15";
+
+$query = $connect->query($req);
+
+if ($query->num_rows > 0){
+
+		while($row = $query->fetch_assoc())
+		{
+			$results[] = array(
+			'label' => $row['recipient'] ,
+			'value' => $row['recipient']
+			);
+		}
+
+echo json_encode($results);
 }
 ?>
