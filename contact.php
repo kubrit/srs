a267b7ace3f78477a7e0a53d3edb75bf4ca248a2
GMBroker <bogumil.kraszewski@gmail.com>
2021-10-21 21:35:10 +0200

    fixed url

--- contact.php
+++ contact.php
@@ -9,7 +9,7 @@ include 'inc/overall/header.php';
 <center>
 <h3><?php echo TXT_APP_DEVELOPER;?></h3>
 <p>Bogumił Kraszewski</p>
-<p>Git: <a href="https://github.com/gmbroker-hq/srs" target="_blank">gmbroker-hq</a></p>
+<p>Git: <a href="https://github.com/kubrit/srs" target="_blank">kubrit</a></p>
 <p></p><br/>
 
 <br/>
